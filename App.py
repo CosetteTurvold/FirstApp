6d02f084bc83d4627907a5f28f3e99d05d65223d
CosetteTurvold <134730528+CosetteTurvold@users.noreply.github.com>
2023-05-30 10:50:11 -0600

    Update App.py

--- App.py
+++ App.py
@@ -10,11 +10,6 @@ instrument_columns = {
     # Add more instruments and their columns as needed here
 }
 
-instrument_rows = {
-    'Density Meter': ['', '', 'x'],
-    'LECO CHN': ['', '', '', ''],
-    # Add more instruments and their columns as needed here
-}
 
 def generate_excel_file(instrument):
     # Logic to generate the Excel file with pre-formatted columns and equations
@@ -22,8 +17,7 @@ def generate_excel_file(instrument):
 
     # Example: Create a DataFrame with the specified columns
     columns = instrument_columns[instrument]
-    rows = instrument_rows[instrument]
-    df = pd.DataFrame(columns=columns, rows=rows)
+    df = pd.DataFrame(columns=columns)
 
     # Add three cells under the column header "O% (diff)" if the instrument is "LECO CHN"
     if instrument == "LECO CHN":
