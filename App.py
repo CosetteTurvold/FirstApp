269dfbf6fcfbf11da579b76dc70828e76cdf5d24
CosetteTurvold <134730528+CosetteTurvold@users.noreply.github.com>
2023-06-26 13:34:26 -0600

    Update App.py

--- App.py
+++ App.py
@@ -6,10 +6,11 @@ import xlsxwriter
 
 # Define the instruments and their corresponding column names
 instrument_columns = {
-    'Density Meter': ['Sample ID', 'Density (g/mL)', 'Temperature °C'],
+    'Density Meter': ['Sample ID', 'Density (g/mL)', 'Temperature (°C)'],
     'LECO CHN': ['Sample ID', 'Mass (g)', 'C%', 'H%', 'N%','O% (diff)'],
-    'Karl Fischer': ['Sample ID','~Vol(uL)', 'Mass(g)', 'Titrant(mL)', 'H2O%'],
+    'Karl Fischer': ['Sample ID','~Vol(μL)', 'Mass(g)', 'Titrant(mL)', 'H2O%'],
     'KF & LECO CHN Combined': ['Sample ID', 'Mass (g)', 'C%', 'H%', 'N%','O% (diff)', 'Water', 'C% Dry Basis', 'H% Dry Basis', 'O% Dry Basis'],
+    'Viscometer': ['Sample ID', 'Viscosity (cP)','Torque (%)','Speed (rpm)', 'Temperature °C'],
     # Add more instruments and their columns as needed here
 }
 
@@ -17,7 +18,6 @@ instrument_columns = {
 def generate_excel_file(instrument):
     # Logic to generate the Excel file with pre-formatted columns and equations
     # You can use libraries like Pandas or openpyxl to work with Excel files
-
     # Example: Create a DataFrame with the specified columns
     columns = instrument_columns[instrument]
     df = pd.DataFrame(columns=columns)
@@ -32,20 +32,16 @@ def generate_excel_file(instrument):
     # Get the workbook from the writer
     workbook = writer.book
     
-    # Define a format for bold cells
+    # Define a format for bold, borders, and green conditional format
     bold_format = writer.book.add_format({'bold': True})
-    
-    border_format = workbook.add_format({'border': 1})
-
-    # Apply borders to columns A to E
-    worksheet.conditional_format('A1:E1048576', {'type': 'no_blanks', 'format': border_format})
-
-    # Apply bottom borders to rows 1 to 7
-    worksheet.conditional_format('A1:E7', {'type': 'blanks', 'format': border_format})
-    
+    bottomborder_format = workbook.add_format({'bottom':2, 'border_color':'black'})
+    rightborder_format = workbook.add_format({'right':2, 'border_color':'black'})
+    headerborder_format = workbook.add_format({'border':2, 'border_color':'black'})
+    cornerborder_format = workbook.add_format({'right':2, 'bottom':2, 'border_color':'black'})
+    green_format = workbook.add_format({'bg_color':   '#C6EFCE',
+                               'font_color': '#006100'})
 
     # Add three cells under the column header "O% (diff)" if the instrument is "LECO CHN"
-
     if instrument == "LECO CHN":
         CHN_calc = [
             ['Sample 1','','','','', "=100-SUM(C2:E2)"],
@@ -57,7 +53,23 @@ def generate_excel_file(instrument):
         ]
         for row in CHN_calc:
             df.loc[len(df)] = row
-            
+
+        # Apply all borders to top row given they are blank OR not blank
+        worksheet.conditional_format('A1:F1', {'type': 'no_blanks', 'format': headerborder_format})
+        worksheet.conditional_format('A1:F1', {'type': 'blanks', 'format': headerborder_format})
+        
+        # Apply bottom borders to A7:E7 given they are blank OR not blank
+        worksheet.conditional_format('A7:E7', {'type': 'no_blanks', 'format': bottomborder_format})
+        worksheet.conditional_format('A7:E7', {'type': 'blanks', 'format': bottomborder_format})
+        
+        # Apply right borders to F2:F6 given they are blank OR not blank
+        worksheet.conditional_format('F2:F6', {'type': 'no_blanks', 'format': rightborder_format})
+        worksheet.conditional_format('F2:F6', {'type': 'blanks', 'format': rightborder_format})
+        
+        # Apply corner border to F7 given it is not blank
+        worksheet.conditional_format('F7', {'type': 'no_blanks', 'format': cornerborder_format})
+        
+        
         # Create an additional sheet and write additional data
         extra_sheetCHN = {
             'Data': ['Additional Data 1', 'Additional Data 2', 'Additional Data 3']
@@ -66,17 +78,35 @@ def generate_excel_file(instrument):
         additional_df.to_excel(writer, index=False, sheet_name='Cresol Testing', startrow=1, startcol=0)
         # Get the worksheet for the Cresol Testing sheet
         worksheet = writer.sheets['Cresol Testing']
-             
-        #additional method apply bold formatting to the cell containing "Additional Data 1" (A1)
-        #worksheet.write('A1', 'Additional Data 1', bold_format)
-        #worksheet.write('A2', 'Additional Data 2', bold_format)
+        
+        #set conditional formatting for Cresol values in range
+        worksheet.conditional_format('B2', {'type':     'cell',
+                                       'criteria': 'between',
+                                       'minimum':  76.86891321,
+                                       'maximum':  78.2112158260259,
+                                       'format':   green_format})
+        worksheet.conditional_format('B3', {'type':     'cell',
+                                       'criteria': 'between',
+                                       'minimum':  7.41272698524775,
+                                       'maximum':  7.96137301475225,
+                                       'format':   green_format})
+        worksheet.conditional_format('B4', {'type':     'cell',
+                                       'criteria': 'between',
+                                       'minimum':  0.0151682547630326,
+                                       'maximum':  0.100665298955976,
+                                       'format':   green_format})
+        worksheet.conditional_format('B5', {'type':     'cell',
+                                       'criteria': 'between',
+                                       'minimum':  13.9828215158141,
+                                       'maximum':  15.5629494519279,
+                                       'format':   green_format})
 
-         # Write new column and row headers in Cresol Testing sheet
+         # Write new column headers in Cresol Testing sheet
         headers = ['Cresol Limit Testing', 'Cresol Measured', 'Average', 'Low-value', 'High-value']
         for i, header in enumerate(headers):
              worksheet.write(0, i, header, bold_format)
              
-             # Write new row headers in Cresol Testing sheet
+        # Write new row headers in Cresol Testing sheet
         row_headers = ['Carbon', 'Hydrogen', 'Nitrogen', 'Oxygen']
         for i, header in enumerate(row_headers):
              worksheet.write(i+1, 0, header, bold_format)
@@ -89,11 +119,10 @@ def generate_excel_file(instrument):
                  ['','', 0.0579167768595041, 0.0151682547630326, 0.100665298955976],
                  ['','', 14.772885483871, 13.9828215158141, 15.5629494519279]
                   ]
-                 
              for row_num, row_data in enumerate(cresol_limits):
                  for col_num, cell_data in enumerate(row_data):
                      worksheet.write(row_num + 1, col_num + 0, cell_data)
-
+                     
         # Get the worksheet for the Cresol Testing sheet
         worksheet = writer.sheets['Cresol Testing']
             
@@ -108,10 +137,26 @@ def generate_excel_file(instrument):
         ]
         for row in kf_calc:
             df.loc[len(df)] = row
-            extra_sheetK = {
+            
+            # Apply all borders to top row given they are blank OR not blank
+            worksheet.conditional_format('A1:E1', {'type': 'no_blanks', 'format': headerborder_format})
+            worksheet.conditional_format('A1:E1', {'type': 'blanks', 'format': headerborder_format})
+            
+            # Apply bottom borders to A7:D7 given they are blank OR not blank
+            worksheet.conditional_format('A7:D7', {'type': 'no_blanks', 'format': bottomborder_format})
+            worksheet.conditional_format('A7:D7', {'type': 'blanks', 'format': bottomborder_format})
+            
+            # Apply right borders to E2:E6 given they are blank OR not blank
+            worksheet.conditional_format('E2:E6', {'type': 'no_blanks', 'format': rightborder_format})
+            worksheet.conditional_format('E2:E6', {'type': 'blanks', 'format': rightborder_format})
+            
+            # Apply corner border to E7 given it is not blank
+            worksheet.conditional_format('E7', {'type': 'no_blanks', 'format': cornerborder_format})
+            
+            extra_sheetKF = {
            'Water Standard Check': ['Water Standard (1)', 'Water Standard (2)', 'Water Standard (3)']
            }
-        additional_df = pd.DataFrame(extra_sheetK)
+        additional_df = pd.DataFrame(extra_sheetKF)
         additional_df.to_excel(writer, index=False, sheet_name='Water Standard', startrow=0, startcol=0)
         worksheet = writer.sheets['Water Standard']
         headers = ['Water Standard Check', 'Mass(g)', 'Titrant(mL)', 'H2O%']
@@ -133,11 +178,6 @@ def generate_excel_file(instrument):
                 worksheet.write(row_num + 4, col_num + 0, cell_data)
         #border_format = workbook.add_format({'border': 1})
 
-        # Apply borders to columns A to E
-        #worksheet.conditional_format('A1:E1048576', {'type': 'no_blanks', 'format': border_format})
-
-        # Apply bottom borders to rows 1 to 7
-        #worksheet.conditional_format('A1:E7', {'type': 'blanks', 'format': border_format})
            
                 
     elif instrument == "KF & LECO CHN Combined":
@@ -151,6 +191,84 @@ def generate_excel_file(instrument):
         ]
         for row in CHN_KF_calc:
             df.loc[len(df)] = row
+            
+            # Apply all borders to top row given they are blank OR not blank
+            worksheet.conditional_format('A1:J1', {'type': 'no_blanks', 'format': headerborder_format})
+            worksheet.conditional_format('A1:J1', {'type': 'blanks', 'format': headerborder_format})
+            
+            
+            # Apply bottom borders to A7:I7 given they are blank OR not blank
+            worksheet.conditional_format('A7:I7', {'type': 'no_blanks', 'format': bottomborder_format})
+            worksheet.conditional_format('A7:I7', {'type': 'blanks', 'format': bottomborder_format})
+            
+            
+            # Apply right borders to J2:J6 given they are blank OR not blank
+            worksheet.conditional_format('J2:J6', {'type': 'no_blanks', 'format': rightborder_format})
+            worksheet.conditional_format('J2:J6', {'type': 'blanks', 'format': rightborder_format})
+            
+            # Apply corner border to J7 given it is blank
+            worksheet.conditional_format('J7', {'type': 'blanks', 'format': cornerborder_format})
+            
+            # Create an additional sheet and write additional data
+            extra_sheetCHNKF = {
+                'Data': ['Additional Data 1', 'Additional Data 2', 'Additional Data 3']
+            }
+        additional_df = pd.DataFrame(extra_sheetCHNKF)
+        additional_df.to_excel(writer, index=False, sheet_name='Cresol Testing', startrow=1, startcol=0)
+            # Get the worksheet for the Cresol Testing sheet
+        worksheet = writer.sheets['Cresol Testing']
+            
+            #set conditional formatting for Cresol values
+        worksheet.conditional_format('B2', {'type':     'cell',
+                                          'criteria': 'between',
+                                          'minimum':  76.86891321,
+                                          'maximum':  78.2112158260259,
+                                          'format':   green_format})
+            
+        worksheet.conditional_format('B3', {'type':     'cell',
+                                          'criteria': 'between',
+                                          'minimum':  7.41272698524775,
+                                          'maximum':  7.96137301475225,
+                                          'format':   green_format})
+            
+        worksheet.conditional_format('B4', {'type':     'cell',
+                                          'criteria': 'between',
+                                          'minimum':  0.0151682547630326,
+                                          'maximum':  0.100665298955976,
+                                          'format':   green_format})
+            
+        worksheet.conditional_format('B5', {'type':     'cell',
+                                          'criteria': 'between',
+                                          'minimum':  13.9828215158141,
+                                          'maximum':  15.5629494519279,
+                                          'format':   green_format})
+                 
+       
+             # Write new column and row headers in Cresol Testing sheet
+        headers = ['Cresol Limit Testing', 'Cresol Measured', 'Average', 'Low-value', 'High-value']
+        for i, header in enumerate(headers):
+            worksheet.write(0, i, header, bold_format)
+                 
+                 # Write new row headers in Cresol Testing sheet
+        row_headers = ['Carbon', 'Hydrogen', 'Nitrogen', 'Oxygen']
+        for i, header in enumerate(row_headers):
+            worksheet.write(i+1, 0, header, bold_format)
+                     
+           # Write Cresol Limits below the columns
+        cresol_limits = [
+            ['','', 77.540064516129, 76.8689132062322, 78.2112158260259], 
+            ['','', 7.68705, 7.41272698524775, 7.96137301475225],
+            ['','', 0.0579167768595041, 0.0151682547630326, 0.100665298955976],
+            ['','', 14.772885483871, 13.9828215158141, 15.5629494519279]
+        ]
+                     
+        for row_num, row_data in enumerate(cresol_limits):
+            for col_num, cell_data in enumerate(row_data):
+                      worksheet.write(row_num + 1, col_num + 0, cell_data)
+                         
+
+            # Get the worksheet for the Cresol Testing sheet
+        worksheet = writer.sheets['Cresol Testing']
     
 
     # Write the DataFrame to the default sheet
@@ -160,7 +278,6 @@ def generate_excel_file(instrument):
                 
     # Close the writer and save the Excel file
     writer.close()
-
     return filename
 
 
